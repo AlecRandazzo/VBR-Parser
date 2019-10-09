68f14bfcb794732502f027d7d3cbd10eda170e8a
AlecRandazzo <alec.randazzo@gmail.com>
2019-10-08 21:13:12 -0400

    first commit, pulled this into its own repo from the GoFor windows collector repo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AlecRandazzo/GoFor-VBR-Parser
+
+go 1.13
+
+require github.com/AlecRandazzo/BinaryTransforms v1.2.1
---
+++ vbr.go
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2019 Alec Randazzo
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package GoFor_VBR_Parser
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	bin "github.com/AlecRandazzo/BinaryTransforms"
+	"math"
+)
+
+type VolumeBootRecord struct {
+	VolumeLetter           string
+	BytesPerSector         int64
+	SectorsPerCluster      int64
+	BytesPerCluster        int64
+	MftByteOffset          int64
+	MftRecordSize          int64
+	ClustersPerIndexRecord int64
+}
+
+// []byte alias containing bytes of a raw volume boot record. Used as a receiver for Parse().
+type RawVolumeBootRecord []byte
+
+// Parses a byte slice containing an NTFS volume boot record (VBR)
+func (rawVolumeBootRecord RawVolumeBootRecord) Parse() (vbr VolumeBootRecord, err error) {
+	// Sanity check that we have the right data
+	sizeOfRawVolumeBootRecord := len(rawVolumeBootRecord)
+	if sizeOfRawVolumeBootRecord == 0 {
+		err = errors.New("RawVolumeBootRecord.Parse() received nil bytes")
+		return
+	} else if sizeOfRawVolumeBootRecord < 512 {
+		err = errors.New("RawVolumeBootRecord.Parse() received less than 512 bytes")
+		return
+	}
+
+	const codeNTFSMagicNumber = "NTFS"
+	const offsetNTFSMagicNumber = 0x03
+	const lengthNTFSMagicNumber = 0x04
+	const offsetBytesPerSector = 0x0b
+	const lengthBytesPerSector = 0x02
+	const offsetSectorsPerCluster = 0x0d
+	const offsetClustersPerMFTRecord = 0x40
+	const offsetMftClusterOffset = 0x30
+	const lengthMftClusterOffset = 0x08
+	const offsetClustersPerIndexRecord = 0x44
+
+	// Sanity check to verify that the function actually received a VBR. Bomb if we didn't.
+	valueNTFSMagicNumber := string(rawVolumeBootRecord[offsetNTFSMagicNumber : offsetNTFSMagicNumber+lengthNTFSMagicNumber])
+	if valueNTFSMagicNumber != codeNTFSMagicNumber {
+		err = errors.New("RawVolumeBootRecord.Parse() received byte slice that does not start with 'NTFS' magic number")
+		return
+	}
+
+	// Start pulling out data based on pre-defined offsets in the VBR
+	valueBytesPerSector := rawVolumeBootRecord[offsetBytesPerSector : offsetBytesPerSector+lengthBytesPerSector]
+	vbr.BytesPerSector = int64(binary.LittleEndian.Uint16(valueBytesPerSector))
+	vbr.SectorsPerCluster = int64(rawVolumeBootRecord[offsetSectorsPerCluster])
+	clustersPerMFTRecord := int(rawVolumeBootRecord[offsetClustersPerMFTRecord])
+	if clustersPerMFTRecord < 128 {
+		err = fmt.Errorf("RawVolumeBootRecord.Parse() found the clusters per MFT record is %d, which is less than 128", clustersPerMFTRecord)
+		vbr = VolumeBootRecord{}
+		return
+	}
+	signedTwosComplement := int8(rawVolumeBootRecord[0x40]) * -1
+	vbr.MftRecordSize = int64(math.Pow(2, float64(signedTwosComplement)))
+	vbr.BytesPerCluster = vbr.SectorsPerCluster * vbr.BytesPerSector
+	valueMftClusterOffset := rawVolumeBootRecord[offsetMftClusterOffset : offsetMftClusterOffset+lengthMftClusterOffset]
+	mftClusterOffset, err := bin.LittleEndianBinaryToInt64(valueMftClusterOffset)
+	if mftClusterOffset == 0 {
+		err = fmt.Errorf("RawVolumeBootRecord.Parse() failed to get mft offset clusters: %w", err)
+		vbr = VolumeBootRecord{}
+		return
+	}
+	vbr.MftByteOffset = mftClusterOffset * vbr.BytesPerCluster
+	vbr.ClustersPerIndexRecord = int64(rawVolumeBootRecord[offsetClustersPerIndexRecord])
+	return
+}
